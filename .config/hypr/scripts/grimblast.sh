7935486dcb33f84c8c28ffba512419595fe7ba24
hudsbls <huds.barreira@gmail.com>
2025-08-07 19:04:30 -0300

    remove all configs file to organize it

--- .config/hypr/scripts/grimblast.sh
+++
@@ -1,5 +0,0 @@
-export time=$(date +%Y-%m-%d_%H:%M:%S)
-
-grimblast --freeze copysave area ~/Pictures/Screenshots/$time.png
-
-dunstify "Screenshot saved" "$time.png" -t 5000 -I ~/Pictures/Screenshots/$time.png -r 0001
